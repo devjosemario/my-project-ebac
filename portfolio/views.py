d59eb08604c9fd3c02e075b27ed40f5aa2d4310a
devjosemario <joosemariovm@gmail.com>
2026-05-12 18:38:50 -0300

    feat: adiciona async view com contador de tempo

--- portfolio/views.py
+++ portfolio/views.py
@@ -14,3 +14,19 @@ def index(request):
 def post_detalhe(request, slug):
     post = Post.objects.get(slug=slug)
     return render(request, 'portfolio/post_detalhe.html', {'post': post})
+
+
+import asyncio
+import time
+from django.http import JsonResponse
+
+
+async def timer_view(request):
+    start = time.time()
+    await asyncio.sleep(2)
+    elapsed = round(time.time() - start, 2)
+    return JsonResponse({
+        "message": "Contagem finalizada!",
+        "tempo_decorrido_segundos": elapsed,
+        "status": "sucesso"
+    })
